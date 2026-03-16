ca4fc3fe46ac56a52203b42f2bd818b9825224b2
the-art-9 <thex10@pratt.edu>
2026-03-16 14:41:25 -0400

    Create page.tsx

---
+++ lib/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../components/ui/button"
+
+export default function Page() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>UI Test</h1>
+      <Button>Test Button</Button>
+    </div>
+  )
+}
