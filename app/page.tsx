b4e36d5571760782d7698409b137ed196371ea63
the-art-9 <thex10@pratt.edu>
2026-03-16 15:01:34 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "../components/ui/button"
-
 export default function Page() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>UI Test</h1>
-      <Button>Test Button</Button>
-    </div>
-  )
+  return <div>Hello Dessn</div>
 }
